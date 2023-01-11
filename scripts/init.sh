3ab0c812e9d7692892a44ab20a610182764fecdb
Kenan Ibrovic <kenan.ibrovic94@gmail.com>
2023-01-11 03:36:39 +0100

    fix init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install terraform
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common gettext
+apt-get update && apt-get install -y gnupg software-properties-common gettext wget unzip
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
   gpg --dearmor | \
-  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 gpg --no-default-keyring \
   --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
@@ -13,14 +13,13 @@ gpg --no-default-keyring \
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
+    tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update && sudo apt-get install terraform
+apt update && apt-get install terraform
 
 # Install aws-cli
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/opt/awscliv2.zip"
+unzip /opt/awscliv2.zip -d /opt/
+bash /opt/aws/install
 
