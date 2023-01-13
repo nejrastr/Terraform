534f5308295e4a36de09cd7bf383b85dc3b7dd23
Kenan Ibrovic <kenan.ibrovic94@gmail.com>
2023-01-13 09:11:05 +0100

    Add private subnet to autoscale group

--- ec2.tf
+++ ec2.tf
@@ -14,42 +14,6 @@ data "aws_ami" "ecs_optimized_amazon_linux_ami" {
   owners = ["591542846629"]
 }
 
-data "aws_ami" "amazon_linux_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  owners = ["137112412989"]
-}
-
-# resource "aws_launch_configuration" "arm_launch_configuration" {
-#   name_prefix     = "arm_config"
-#   image_id        = data.aws_ami.ecs_optimized_amazon_linux_ami.id
-#   instance_type   = "t3.micro"
-#   key_name        = aws_key_pair.arm_ec2_access_key.key_name
-#   security_groups = [aws_security_group.arm_security_group.id]
-#   iam_instance_profile = aws_iam_instance_profile.ecs_role.name
-#   user_data = templatefile("./templates/user_data.tpl",{
-#     cluster_name = aws_ecs_cluster.arm_ecs_cluster.name
-#   })
-
-#   root_block_device {
-#     encrypted = true
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
 resource "aws_launch_template" "arm_launch_template" {
   name_prefix            = "arm_launch_template"
   image_id               = data.aws_ami.ecs_optimized_amazon_linux_ami.id
@@ -75,10 +39,6 @@ resource "aws_launch_template" "arm_launch_template" {
     name = aws_iam_instance_profile.ecs_instance_role.name
   }
 
-  # monitoring {
-  #   enabled = true
-  # }
-
   depends_on = [
     aws_kms_key.ebs_encryption_key
   ]
@@ -90,9 +50,9 @@ resource "aws_launch_template" "arm_launch_template" {
 
 resource "aws_autoscaling_group" "arm_autoscaling_group" {
   name_prefix               = "arm_autoscaling_group"
-  max_size                  = 1
-  min_size                  = 1
-  vpc_zone_identifier       = [aws_subnet.arm_subnet_public.id]
+  max_size                  = 2
+  min_size                  = 2
+  vpc_zone_identifier       = [aws_subnet.arm_subnet_public.id, aws_subnet.arm_subnet_private.id]
   wait_for_capacity_timeout = "2m"
 
   launch_template {
@@ -104,16 +64,3 @@ resource "aws_autoscaling_group" "arm_autoscaling_group" {
     aws_launch_template.arm_launch_template
   ]
 }
-
-# resource "aws_instance" "arm_server_private" {
-#   ami                    = data.aws_ami.amazon_linux_ami.id
-#   instance_type          = "t3.micro"
-#   key_name               = aws_key_pair.arm_ec2_access_key.key_name
-#   vpc_security_group_ids = [resource.aws_security_group.arm_security_group.id]
-#   subnet_id              = aws_subnet.arm_subnet_private.id
-
-#   root_block_device {
-#     encrypted  = true
-#     kms_key_id = resource.aws_kms_key.ebs_encryption_key.arn
-#   }
-# }
