690b3402d2b61f0ff1235e25ccedfd46a8d59ba9
Kenan Ibrovic <kenan.ibrovic94@gmail.com>
2023-01-13 14:28:31 +0100

    Update tasks

--- ecs.tf
+++ ecs.tf
@@ -9,69 +9,115 @@ resource "aws_ecs_cluster" "arm_ecs_cluster" {
 }
 
 resource "aws_ecs_task_definition" "frontend_task" {
-  family                   = "frontend-task"
-  network_mode             = "bridge"
-  requires_compatibilities = ["EC2"]
-  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "nginx"
-      cpu       = 200
-      memory    = 200
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode(
+    [
+      {
+        command    = []
+        cpu        = 256
+        entryPoint = []
+        environment = [
+          {
+            name  = "MYSQL_DB"
+            value = aws_instance.arm_server_private.private_dns
+          },
+        ]
+        essential   = true
+        image       = "kibrovic/frontend-task"
+        memory      = 512
+        mountPoints = []
+        name        = "frontend-task"
+        portMappings = [
+          {
+            containerPort = 8080
+            hostPort      = 80
+            protocol      = "tcp"
+          },
+        ]
+        volumesFrom = []
+      },
+    ]
+  )
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  family             = "frontend-task"
+  requires_compatibilities = [
+    "EC2",
+  ]
+  task_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
   placement_constraints {
-    type       = "memberOf"
     expression = "attribute:ecs.subnet-id in [${aws_subnet.arm_subnet_public.id}]"
+    type       = "memberOf"
   }
 }
 
 resource "aws_ecs_service" "frontend_service" {
-  name            = "frontend-service"
-  cluster         = aws_ecs_cluster.arm_ecs_cluster.id
-  task_definition = aws_ecs_task_definition.frontend_task.arn
-  desired_count   = 1
+  cluster                            = aws_ecs_cluster.arm_ecs_cluster.id
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
+  desired_count                      = 1
+  name                               = "frontend-service"
+  task_definition                    = aws_ecs_task_definition.frontend_task.arn
 }
 
-resource "aws_ecs_task_definition" "backend_task" {
-  family                   = "backend-task"
-  network_mode             = "bridge"
-  requires_compatibilities = ["EC2"]
-  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "nginx"
-      cpu       = 200
-      memory    = 200
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 8080
+resource "aws_ecs_task_definition" "database_task" {
+  container_definitions = jsonencode(
+    [
+      {
+        command    = []
+        cpu        = 256
+        entryPoint = []
+        environment = [
+          {
+            name  = "MYSQL_DATABASE"
+            value = "DBWT19"
+          },
+          {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = "root"
+          },
+        ]
+        essential = true
+        healthCheck = {
+          command = [
+            "mysqladmin ping -h localhost",
+          ]
+          interval = 30
+          retries  = 10
+          timeout  = 20
         }
-      ]
-    }
-  ])
+        image       = "mysql"
+        memory      = 512
+        mountPoints = []
+        name        = "database"
+        portMappings = [
+          {
+            containerPort = 3306
+            hostPort      = 3306
+            protocol      = "tcp"
+          },
+        ]
+        volumesFrom = []
+      },
+    ]
+  )
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  family             = "database-task"
+  requires_compatibilities = [
+    "EC2",
+  ]
+  task_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
   placement_constraints {
-    type       = "memberOf"
     expression = "attribute:ecs.subnet-id in [${aws_subnet.arm_subnet_private.id}]"
+    type       = "memberOf"
   }
 }
 
-resource "aws_ecs_service" "backend_service" {
-  name            = "backend-service"
-  cluster         = aws_ecs_cluster.arm_ecs_cluster.id
-  task_definition = aws_ecs_task_definition.backend_task.arn
-  desired_count   = 1
+resource "aws_ecs_service" "database_service" {
+  cluster                            = aws_ecs_cluster.arm_ecs_cluster.id
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
+  desired_count                      = 1
+  name                               = "database-service"
+  task_definition                    = aws_ecs_task_definition.database_task.arn
 }
