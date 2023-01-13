96a4fde9fc9d9f2c445deee4d73876a87dd5ecee
Kenan Ibrovic <kenan.ibrovic94@gmail.com>
2023-01-13 09:13:08 +0100

    Add nat for private subnet

--- network.tf
+++ network.tf
@@ -37,6 +37,34 @@ resource "aws_route_table_association" "arm_rt_public_subnet" {
   route_table_id = aws_route_table.arm_public_rt.id
 }
 
+resource "aws_eip" "arm_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "arm_nat_gateway" {
+  allocation_id = aws_eip.arm_eip.id
+  subnet_id     = aws_subnet.arm_subnet_public.id
+
+  depends_on = [
+    aws_internet_gateway.arm_igw
+  ]
+}
+
+resource "aws_route_table" "arm_private_rt" {
+  vpc_id = aws_vpc.arm_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.arm_nat_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "arm_rt_private_subnet" {
+  subnet_id      = aws_subnet.arm_subnet_private.id
+  route_table_id = aws_route_table.arm_private_rt.id
+}
+
+
 resource "aws_security_group" "arm_security_group" {
   name        = "arm_security_group"
   description = "Security group for ARM EC2 instance"
